76aeee38aaf2977f20bf0ad60b58fbeb219fa236
roshanadke <roshan.adke.ext@wsa.com>
2026-03-16 20:23:03 +0530

    refresh bugfix

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,3 +1,13 @@
 import { pingPageMap } from './seoContent'
 
-export const getRouteForPath = (pathname) => pingPageMap[pathname] ?? pingPageMap['/ping-test']
+export const normalizePath = (pathname) => {
+  if (!pathname) {
+    return '/'
+  }
+
+  const normalized = pathname.endsWith('/') && pathname !== '/' ? pathname.slice(0, -1) : pathname
+  return normalized || '/'
+}
+
+export const getRouteForPath = (pathname) =>
+  pingPageMap[normalizePath(pathname)] ?? pingPageMap['/ping-test']
